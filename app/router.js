b4a1fc892c0dfdf265dd9e5c7fdd3cb0e7231377
Jason Ohr <jason.ohr@gmail.com>
2015-07-01 09:00:40 -0400

    added several files to get this to work tho I need to look over things and make some changes

--- app/router.js
+++ app/router.js
@@ -6,7 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('pins', function() {});
+  this.route('pins', function() {
+    this.route('add-image-portal');
+  });
 });
 
 export default Router;
--- app/routes/pins/index.js
+++ app/routes/pins/index.js
@@ -2,10 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    //return [
-    //  {caption: 'hello dude', url: 'http://google.com'},
-    //  {caption: 'sweet momba jamba', url: 'http://jasonohr.com'}
-    //]
+
     return this.store.findAll('pin');
   }
 });
