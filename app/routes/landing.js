060b204a3d7d75fa997f5658282409587d524666
Jason Ohr <jason.ohr@gmail.com>
2015-07-01 19:45:07 -0400

    oops, forgot to add all the extra files for an unknown # of commits

---
+++ app/routes/landing.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+//The landing page for the app
+export default Ember.Route.extend({
+
+});
---
+++ app/routes/users/create-portal.js
@@ -0,0 +1,4 @@
+import Ember from 'ember';
+//users create user portal
+export default Ember.Route.extend({
+});
